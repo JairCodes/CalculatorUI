34e372438b85251cec9988137011475bd944b32f
Jair Ramirez <jairramirez0813@gmail.com>
2023-11-09 20:25:40 -0600

    Implemented Calculator into CalculatorUI
    fixed issue

--- app/src/main/java/calculatordemo2/CalculatorUI.java
+++ app/src/main/java/calculatordemo2/CalculatorUI.java
@@ -1,42 +1,46 @@
 package calculatordemo2;
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
-import javax.swing.JTextArea;
-import javax.swing.JPanel;
 
+import javax.swing.*;
 
 public class CalculatorUI {
     private final JFrame frame;
     private final PanelFactory panelFactory;
     private final ButtonFactory buttonFactory;
     private final JTextArea textArea;
+    private final Calculator calculator;
 
     public CalculatorUI() {
         frame = new JFrame("Calculator");
-		frame.setResizable(true);
-		textArea = new JTextArea(2,25);
+        frame.setResizable(true);
+        textArea = new JTextArea(2, 25);
         panelFactory = new PanelFactory();
         buttonFactory = new ButtonFactory();
+        calculator = new Calculator(); // Instantiate the Calculator
     }
 
     public void init() {
-    frame.setSize(400, 300);
-    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-    JPanel panel = panelFactory.createPanel();
-    JPanel numericButtonsPanel = panelFactory.createPanelForNumericButtons(buttonFactory, textArea);
-    JPanel primitiveOperatorsPanel = panelFactory.createPrimitiveOperatorsPanel(buttonFactory);
-    JPanel trigOperatorsPanel = panelFactory.createTrigonometricOperatorsPanel(buttonFactory);
-    JPanel commonFunctionOperatorsPanel = panelFactory.createCommonFunctionOperatorsPanel(buttonFactory);
-    
-    panel.add(textArea); // Add the text area to the panel
-    panel.add(numericButtonsPanel);
-    panel.add(primitiveOperatorsPanel);
-    panel.add(trigOperatorsPanel);
-    panel.add(commonFunctionOperatorsPanel);
+        JPanel panel = panelFactory.createPanel();
+        JPanel numericButtonsPanel = panelFactory.createPanelForNumericButtons(buttonFactory, textArea);
+        JPanel primitiveOperatorsPanel = panelFactory.createPrimitiveOperatorsPanel(buttonFactory, textArea, calculator);
+        JPanel trigOperatorsPanel = panelFactory.createTrigonometricOperatorsPanel(buttonFactory, textArea, calculator);
+        JPanel commonFunctionOperatorsPanel = panelFactory.createCommonFunctionOperatorsPanel(buttonFactory, textArea, calculator);
 
-    frame.add(panel);
-    frame.setVisible(true);
-	}
+        panel.add(textArea);
+        panel.add(numericButtonsPanel);
+        panel.add(primitiveOperatorsPanel);
+        panel.add(trigOperatorsPanel);
+        panel.add(commonFunctionOperatorsPanel);
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+
+    public void updateTextArea(Double result) {
+        if (!Double.isNaN(result)) {
+            textArea.setText(Double.toString(result));
+        }
+    }
 }
-
