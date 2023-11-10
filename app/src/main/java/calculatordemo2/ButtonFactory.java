fca9cc310f67b0250060058d3d3ff350d269e588
Jair Ramirez <jairramirez0813@gmail.com>
2023-11-09 20:26:46 -0600

    Implemented Calculator into ButtonFactory *Errors in Primitve Type*
    NEEDS FIXING

--- app/src/main/java/calculatordemo2/ButtonFactory.java
+++ app/src/main/java/calculatordemo2/ButtonFactory.java
@@ -1,6 +1,7 @@
 package calculatordemo2;
-// ButtonFactory.java
-import javax.swing.*;
+//ButtonFactory
+import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 public class ButtonFactory {
     public JButton createNumericButton(int value, JTextArea textArea) {
@@ -10,6 +11,7 @@ public class ButtonFactory {
         });
         return button;
     }
+
     public JButton[] createNumericButtons(JTextArea textArea) {
         JButton[] numericButtons = new JButton[10];
         for (int i = 0; i < 10; i++) {
@@ -17,47 +19,139 @@ public class ButtonFactory {
         }
         return numericButtons;
     }
-    public JButton createNumericButton(int value) {
-        return new JButton(String.valueOf(value));
+
+    public JButton createOperatorButton(String label, JTextArea textArea, Calculator calculator) {
+        JButton button = new JButton(label);
+        button.addActionListener(e -> {
+            String currentText = textArea.getText();
+
+            if (label.equals("=")) {
+                Double result = calculator.calculateEqual(Double.valueOf(currentText));
+                textArea.setText(String.valueOf(result));
+            } else {
+                currentText += " " + label + " ";
+                textArea.setText(currentText);
+            }
+        });
+        return button;
     }
 
-    public JButton createOperatorButton(String label) {
-        return new JButton(label);
+    public JButton createNumericButton(int value, JTextArea textArea, Calculator calculator) {
+        JButton button = new JButton(String.valueOf(value));
+        button.addActionListener(e -> {
+            textArea.append(String.valueOf(value)); // Append the value to the text area
+            double numericValue = Double.parseDouble(textArea.getText());
+            double result = calculator.calcScience(Calculator.singleOperator.squareRoot, numericValue);
+            textArea.setText(String.valueOf(result)); // Display the square root of the entered number
+        });
+        return button;
+    }
+    
+    public JButton createPrimitiveOperatorButton(String label, JTextArea textArea, Calculator calculator) {
+        JButton button = new JButton(label);
+        button.addActionListener(e -> {
+            Double result = null;
+    
+            switch (label) {
+                case "+":
+                    result = calculator.twoOpCaller(Calculator.twoOperator.add, Double.parseDouble(textArea.getText()));
+                    break;
+                case "-":
+                    result = calculator.twoOpCaller(Calculator.twoOperator.subtract, Double.parseDouble(textArea.getText()));
+                    break;
+                case "*":
+                    result = calculator.twoOpCaller(Calculator.twoOperator.multiply, Double.parseDouble(textArea.getText()));
+                    break;
+                case "/":
+                    result = calculator.twoOpCaller(Calculator.twoOperator.divide, Double.parseDouble(textArea.getText()));
+                    break;
+                // Add more cases for other arithmetic operations if needed
+                default:
+                    break;
+            }
+    
+            if (result != null) {
+                textArea.setText(String.valueOf(result));
+            }
+        });
+        return button;
     }
 
-    public JButton createTrigonometricOperatorButton(String label) {
-        return new JButton(label);
-        // Specific handling for trigonometric operators
+    public JButton createCommonFunctionOperatorButton(String label, JTextArea textArea, Calculator calculator) {
+        JButton button = new JButton(label);
+        button.addActionListener(e -> {
+            Double result = null;
+
+            switch (label) {
+                case "√":
+                    result = calculator.calcScience(Calculator.singleOperator.squareRoot, Double.parseDouble(textArea.getText()));
+                    break;
+                case "x^2":
+                    result = calculator.calcScience(Calculator.singleOperator.square, Double.parseDouble(textArea.getText()));
+                    break;
+                case "1/x":
+                    result = calculator.calcScience(Calculator.singleOperator.oneDevidedBy, Double.parseDouble(textArea.getText()));
+                    break;
+                // Add additional cases for other functionalities if needed
+                default:
+                    break;
+            }
+
+            if (result != null) {
+                textArea.setText(String.valueOf(result));
+            }
+        });
+        return button;
+    }
+    public JButton createTrigonometricOperatorButton(String label, JTextArea textArea, Calculator calculator) {
+        JButton button = new JButton(label);
+        button.addActionListener(e -> {
+            Double result = null;
+            switch (label) {
+                case "Sin":
+                    result = calculator.calcScience(Calculator.singleOperator.sin, Double.parseDouble(textArea.getText()));
+                    break;
+                case "Cos":
+                    result = calculator.calcScience(Calculator.singleOperator.cos, Double.parseDouble(textArea.getText()));
+                    break;
+                case "Tan":
+                    result = calculator.calcScience(Calculator.singleOperator.tan, Double.parseDouble(textArea.getText()));
+                    break;
+                // Add additional cases for other trigonometric functions if needed
+                default:
+                    break;
+            }
+
+            if (result != null) {
+                textArea.setText(String.valueOf(result));
+            }
+        });
+        return button;
     }
 
-    public JButton createCommonFunctionOperatorButton(String label) {
-        return new JButton(label);
-        // Specific handling for common function operators
-    }
-
-    public JButton[] createPrimitiveOperatorButtons() {
+    public JButton[] createPrimitiveOperatorButtons(JTextArea textArea, Calculator calculator) {
         JButton[] primitiveOperatorButtons = new JButton[5];
-        primitiveOperatorButtons[0] = createOperatorButton("+");
-        primitiveOperatorButtons[1] = createOperatorButton("-");
-        primitiveOperatorButtons[2] = createOperatorButton("*");
-        primitiveOperatorButtons[3] = createOperatorButton("/");
-        primitiveOperatorButtons[4] = createOperatorButton("=");
+        primitiveOperatorButtons[0] = createOperatorButton("+", textArea, calculator);
+        primitiveOperatorButtons[1] = createOperatorButton("-", textArea, calculator);
+        primitiveOperatorButtons[2] = createOperatorButton("*", textArea, calculator);
+        primitiveOperatorButtons[3] = createOperatorButton("/", textArea, calculator);
+        primitiveOperatorButtons[4] = createOperatorButton("=", textArea, calculator);
         return primitiveOperatorButtons;
     }
 
-    public JButton[] createTrigonometricOperatorButtons() {
+    public JButton[] createTrigonometricOperatorButtons(JTextArea textArea, Calculator calculator) {
         JButton[] trigonometricOperatorButtons = new JButton[3];
-        trigonometricOperatorButtons[0] = createTrigonometricOperatorButton("Sin");
-        trigonometricOperatorButtons[1] = createTrigonometricOperatorButton("Cos");
-        trigonometricOperatorButtons[2] = createTrigonometricOperatorButton("Tan");
+        trigonometricOperatorButtons[0] = createTrigonometricOperatorButton("Sin", textArea, calculator);
+        trigonometricOperatorButtons[1] = createTrigonometricOperatorButton("Cos", textArea, calculator);
+        trigonometricOperatorButtons[2] = createTrigonometricOperatorButton("Tan", textArea, calculator);
         return trigonometricOperatorButtons;
     }
 
-    public JButton[] createCommonFunctionOperatorButtons() {
+    public JButton[] createCommonFunctionOperatorButtons(JTextArea textArea, Calculator calculator) {
         JButton[] commonFunctionOperatorButtons = new JButton[3];
-        commonFunctionOperatorButtons[0] = createCommonFunctionOperatorButton("√");
-        commonFunctionOperatorButtons[1] = createCommonFunctionOperatorButton("x^2");
-        commonFunctionOperatorButtons[2] = createCommonFunctionOperatorButton("1/x");
+        commonFunctionOperatorButtons[0] = createCommonFunctionOperatorButton("√", textArea, calculator);
+        commonFunctionOperatorButtons[1] = createCommonFunctionOperatorButton("x^2", textArea, calculator);
+        commonFunctionOperatorButtons[2] = createCommonFunctionOperatorButton("1/x", textArea, calculator);
         return commonFunctionOperatorButtons;
     }
 }
